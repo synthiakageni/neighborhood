319432ce6f065d3b0a90d2ac51ab1318ca00d877
synthiakageni <synthia.kageni@student.moringaschool.com>
2022-01-10 10:25:01 +0300

    add django_bootstrap4 as an installed app

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'mambotokea',
+    'django_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+AUTH_USER_MODEL = 'mambotokea.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
