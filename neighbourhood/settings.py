19b2af44d9fc6ec613bafa4f32bc5cd4c5a1211f
synthiakageni <synthia.kageni@student.moringaschool.com>
2022-01-10 09:28:07 +0300

    Change project database settings to use Postgres

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -75,9 +75,11 @@ WSGI_APPLICATION = 'neighbourhood.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hood',
+        'USER': 'synthia',
+    'PASSWORD':'123',
     }
 }
 
